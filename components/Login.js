bfca1b0911a1b714804016eec38689f0fbead1bb
SeungJin Kim <ksj_1207@hotmail.com>
2021-10-11 13:02:07 +0200

    Login page build with styled components

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { View } from 'react-native'
 import { Formik } from 'formik'
+import { Octicons, Ionicons } from '@expo/vector-icons'
 //logo
 import hanmaumLogo from '../assets/hanmaum-logo.png'
 
@@ -12,9 +14,50 @@ import {
   PageLogo,
   PageTitle,
   SubTitle,
+  StyledFormArea,
+  StyledInputLabel,
+  LeftIcon,
+  RightIcon,
+  StyledTextInput,
+  StartButton,
+  ButtonText,
+  Colors,
 } from '../appStyles/appStyles'
 
+//Colors
+const { brand, darkLight } = Colors
+
+const TextInput = ({
+  label,
+  icon,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Ionicons
+            name={hidePassword ? 'md-eye-off' : 'md-eye'}
+            size={30}
+            color={darkLight}
+          />
+        </RightIcon>
+      )}
+    </View>
+  )
+}
+
 const Login = () => {
+  const [hidePassword, setHidePassword] = useState(true)
+
   return (
     <Container>
       <StatusBar style='dark' />
@@ -28,7 +71,38 @@ const Login = () => {
           onSubmit={(values) => {
             console.log(values)
           }}
-        ></Formik>
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <StyledFormArea>
+              <TextInput
+                label='Email Adress'
+                icon='mail'
+                placeholder='a@gmail.com'
+                placeholderTextColor={darkLight}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                keyboardType='email-address'
+                onChangeText={handleChange('email')}
+              />
+              <TextInput
+                label='Password'
+                icon='lock'
+                placeholder='*********'
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+              <StartButton onPress={handleSubmit}>
+                <ButtonText>Login</ButtonText>
+              </StartButton>
+            </StyledFormArea>
+          )}
+        </Formik>
       </InnerContainer>
     </Container>
   )
