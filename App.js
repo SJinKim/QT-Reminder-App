c4668f29b7af4235ccebcc12c88cce9c30033e93
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-09 02:49:07 +0100

    not used screen removed

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 //Components
 import Home from './screen/Home'
@@ -11,18 +9,28 @@ import Signup from './screen/Signup'
 import TimerDisplay from './components/TimerDisplay'
 import TimerSet from './components/TimerSet'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false}} name="Login" component={Login} />
-        <Stack.Screen options={{ headerShown: false}} name="Signup" component={Signup} />
-        <Stack.Screen options={{ headerShown: false}} name="Home" component={Home} />
-        <Stack.Screen options={{ headerShown: false}} name="Countdown" component={CountDown} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='Login'
+          component={Login}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='Signup'
+          component={Signup}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='Home'
+          component={Home}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
-
 }
