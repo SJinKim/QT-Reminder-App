7b75703583b54bdd39f223628b603789a8fffe07
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-11 03:06:16 +0100

    screens which need changes

---
+++ screen/AllScreens.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ScreenContainer } from 'react-native-screens'
+import Screens from '../screen/Screens'
+
+//needs to be adjusted
+export const ProfileScreen = ({ pages }) => <Screens pages='Profile' />
+export const MessageScreen = ({ pages }) => <Screens pages='Message' />
+export const StatisticScreen = ({ pages }) => <Screens pages='Statistic' />
