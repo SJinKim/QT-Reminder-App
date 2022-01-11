bdf8d8f789358e3a96e2c9b4d675900052365ba9
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-11 03:08:03 +0100

    deleted files + changed to yarn from npm

--- components/Data.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Data = () => {
-  return <div></div>
-}
--- components/TimerHeader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-const TimerHeader = () => {
-  return <Text>TimerHeader</Text>
-}
-
-export default TimerHeader
