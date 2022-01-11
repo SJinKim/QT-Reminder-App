229200cf6521ec7d3f6de7eba6cb8cba5fbb1429
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-11 03:06:46 +0100

    loading screen added for better visual

---
+++ screen/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Loading = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Loading...</Text>
+    </View>
+  )
+}
+
+export default Loading
