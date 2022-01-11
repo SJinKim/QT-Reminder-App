9d7c311b588119883b94edf7ddb7584028ccdd81
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-11 03:04:31 +0100

    final change for v1

--- App.js
+++ App.js
@@ -1,36 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { auth } from './firebase'
+import { AuthContext } from './context'
+
+//screen
+import Loading from './screen/Loading'
+
+import Menu from './navigation/Drawer'
+import AuthStack from './navigation/AuthStack'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-//Components
-import Home from './screen/Home'
-import Login from './screen/Login'
-import Signup from './screen/Signup'
-import TimerDisplay from './components/TimerDisplay'
-import TimerSet from './components/TimerSet'
-
-const Stack = createNativeStackNavigator()
+// const RootStack = createNativeStackNavigator()
+// const RootStackScreen = ({ userToken }) => (
+//   <RootStack.Navigator screenOptions={{ headerShown: false }}>
+//     {userToken ? (
+//       <RootStack.Screen name='Auth' component={AuthStack} />
+//     ) : (
+//       <RootStack.Screen name='Main' component={Menu} />
+//     )}
+//   </RootStack.Navigator>
+// )
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [userToken, setUserToken] = useState(null)
+
+  const authContext = useMemo(() => {
+    return {
+      login: () => {
+        setIsLoading(false)
+        setUserToken(auth.currentUser)
+      },
+      signUp: () => {
+        setIsLoading(false)
+        setUserToken(auth.currentUser)
+      },
+      signOut: () => {
+        setIsLoading(false)
+        setUserToken(null)
+      },
+    }
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name='Login'
-          component={Login}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name='Signup'
-          component={Signup}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name='Home'
-          component={Home}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {userToken ? <Menu /> : <AuthStack />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   )
 }
