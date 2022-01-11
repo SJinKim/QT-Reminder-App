663ef749abc3a6f47514f1cd3cb4ee3f76987e1a
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-11 03:06:03 +0100

    label changed

--- components/DropdownMenus.js
+++ components/DropdownMenus.js
@@ -4,7 +4,7 @@ export const churches = [
   { label: 'Mannheim', value: 'Mannheim' },
 ]
 
-export const belong = [
+export const cells = [
   { label: '청년', value: '청년' },
   { label: '초중고', value: '초중고' },
   { label: '장년', value: '장년' },
